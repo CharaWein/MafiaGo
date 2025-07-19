92c88c619c003d882ab6f3c39be4ddecdb30bbac
CharaWein <Weinmaier-Andrew@yandex.ru>
2025-07-19 17:42:33 +0300

    restart____

--- go.mod
+++ go.mod
@@ -1,8 +1,10 @@
-module github.com/CharaWein/mafia-game
+module go-online-game
 
 go 1.21
 
 require (
-	github.com/gorilla/mux v1.8.0
-	github.com/gorilla/websocket v1.5.3
+	github.com/google/uuid v1.6.0
+	github.com/gorilla/websocket v1.5.1
 )
+
+require golang.org/x/net v0.17.0 // indirect
--- main.go
+++ main.go
@@ -1,25 +1,108 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"sync"
 
-	"github.com/CharaWein/mafia-game/handlers"
-	"github.com/gorilla/mux"
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true // В продакшене замените на проверку origin!
+	},
+}
+
+// Room представляет лобби/комнату
+type Room struct {
+	ID      string
+	Players map[*websocket.Conn]bool
+	mu      sync.Mutex
+}
+
+var rooms = make(map[string]*Room)
+var roomsMu sync.Mutex
+
 func main() {
-	r := mux.NewRouter()
-	h := handlers.NewHandler()
+	http.HandleFunc("/ws", handleWebSocket)
+	http.HandleFunc("/create", createRoom)
+	http.HandleFunc("/list", listRooms)
 
-	// Настройка маршрутов
-	r.HandleFunc("/create", h.CreateGame).Methods("POST")
-	r.HandleFunc("/join/{gameID}", h.JoinGame).Methods("POST")
-	r.HandleFunc("/lobby/{gameID}", h.GetLobbyState).Methods("GET")
-	r.HandleFunc("/ws", h.WebSocketHandler)
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./static")))
+	log.Println("Сервер запущен на :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
-	port := ":8080"
-	log.Printf("Server starting on port %s", port)
-	log.Fatal(http.ListenAndServe(port, r))
+// Создание новой комнаты
+func createRoom(w http.ResponseWriter, r *http.Request) {
+	roomID := uuid.New().String()
+
+	roomsMu.Lock()
+	rooms[roomID] = &Room{
+		ID:      roomID,
+		Players: make(map[*websocket.Conn]bool),
+	}
+	roomsMu.Unlock()
+
+	json.NewEncoder(w).Encode(map[string]string{"roomID": roomID})
+}
+
+// Список всех комнат
+func listRooms(w http.ResponseWriter, r *http.Request) {
+	roomsMu.Lock()
+	defer roomsMu.Unlock()
+
+	var roomIDs []string
+	for id := range rooms {
+		roomIDs = append(roomIDs, id)
+	}
+
+	json.NewEncoder(w).Encode(map[string][]string{"rooms": roomIDs})
+}
+
+// Обработчик WebSocket
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Ошибка WebSocket:", err)
+		return
+	}
+	defer conn.Close()
+
+	roomID := r.URL.Query().Get("roomID")
+	if roomID == "" {
+		log.Println("Не указан roomID")
+		return
+	}
+
+	roomsMu.Lock()
+	room, exists := rooms[roomID]
+	roomsMu.Unlock()
+
+	if !exists {
+		log.Println("Комната не найдена")
+		return
+	}
+
+	// Добавляем игрока в комнату
+	room.mu.Lock()
+	room.Players[conn] = true
+	room.mu.Unlock()
+
+	log.Printf("Игрок подключился к комнате %s", roomID)
+
+	// Чтение сообщений от игрока (можно расширить)
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Игрок отключился:", err)
+			room.mu.Lock()
+			delete(room.Players, conn)
+			room.mu.Unlock()
+			break
+		}
+		log.Printf("Сообщение от игрока в комнате %s: %s", roomID, string(msg))
+	}
 }
